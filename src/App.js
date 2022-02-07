29b6093cf58ff4c7eae76e0e74aba0e136c02dc6
dangulo <dangulo@cofrico.com>
2022-02-07 13:10:23 +0100

    Commit 1: Comienzo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React, { Component } from "react";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import NavBar from "./components/navBar/NavBar";
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <div><NavBar/></div>
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
